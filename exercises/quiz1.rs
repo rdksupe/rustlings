3f31418891d04a3075bb16f309279efc3ffc97a1
Rishi <rishikirti534@gmail.com>
2024-05-15 02:45:25 +0530

    primitive types solved

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,26 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x:i32)-> i32 {
+    let answers = [70,80,41,65] ;
+    if x == 35 {
+        answers[0] 
+    }
+
+    else if x == 40 {
+        answers[1] 
+    }
+
+    else if x == 41 {
+        answers[2]
+    }
+
+    else {
+        answers[3]
+    }
+}
 
 // Don't modify this function!
 #[test]
